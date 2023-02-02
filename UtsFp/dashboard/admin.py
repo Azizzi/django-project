5441698a7b07db3616d92edebbf0e265d50c0e1d
Azizzi <abdulazizhadi726@gmail.com>
2023-02-02 18:16:32 +0700

    first commit

--- UtsFp/dashboard/admin.py
+++ UtsFp/dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import komik, Jenis
 from .models import komik
 
 admin.site.register(komik)
@@ -8,3 +9,4 @@ admin.site.register(komik)
 from .models import daftarkomik
 
 admin.site.register(daftarkomik)
+admin.site.register(Jenis)
--- UtsFp/dashboard/models.py
+++ UtsFp/dashboard/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Jenis(models.Model):
+    nama=models.CharField(max_length=50)
+    ket=models.TextField()
+
+    def __str__(self):
+        return self.nama
+
 class komik(models.Model):
     kodekomik = models.CharField(max_length=8)
     namakomik = models.CharField(max_length=120)
     penerbit = models.CharField(max_length=50)
     stockkomik = models.IntegerField()
     hargakomik = models.BigIntegerField()
+    jenis_id=models.ForeignKey(Jenis, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.namakomik
@@ -15,6 +23,7 @@ class daftarkomik(models.Model):
     kodekomik = models.CharField(max_length=8)
     namakomik = models.CharField(max_length=120)
     penerbit = models.CharField(max_length=50)
+    jenis_id=models.ForeignKey(Jenis, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return "{}. {}".format(self.kodekomik,self.namakomik)
